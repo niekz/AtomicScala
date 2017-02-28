b51bee758631ada082df95d176e12667ac74b695
johannventer2@gmail.com <rand0m>
2017-02-28 16:04:44 +0200

    AtomicScala Exercises - Added FunctionsAsObjects file. Added default main function. Example code to follow. Exercise code to follow.

---
+++ AtomicScala3FunctionsAsObjects.scala
@@ -0,0 +1,13 @@
+/**
+  * Created by johann on 2017/02/28.
+  */
+
+import com.atomicscala.AtomicTest._
+
+object AtomicScala3FunctionsAsObjects {
+  def main(args: Array[String]): Unit = {
+
+  }
+
+
+}
