6aca1f7a61384673fd6bd602090087e3e8b5cf38
johannventer2@gmail.com <rand0m>
2017-02-28 16:03:18 +0200

    AtomicScala Exercises - Added exercise code for functions of the chapter. Added test cases for testing the functions.

--- AtomicScala3ParameterizedTypes.scala
+++ AtomicScala3ParameterizedTypes.scala
@@ -14,5 +14,80 @@ object AtomicScala3ParameterizedTypes {
 
     v1 is p1
     v2 is p2
+
+    explicitDouble(1.0, 2.0, 3.0) is Vector(1.0, 2.0, 3.0)
+
+    explicitList(Vector(10.0, 20.0)) is List(10.0, 20.0)
+    explicitList(Vector(1, 2, 3)) is List(1.0, 2.0, 3.0)
+
+    explicitSet(Vector(10.0, 20.0)) is Set(10.0, 20.0)
+    explicitSet(Vector(1, 2, 3, 2, 3, 4)) is Set(1.0, 2.0, 3.0, 4.0)
+
+    val weather = Vector(100, 80, 20, 100, 20)
+    historicalData(weather) is "Sunny=2, Mostly Sunny=1, Partly Sunny=0, Mostly Cloudy=2, Cloudy=0"
+
+
   }
+
+  def inferred(c1: Char, c2: Char, c3: Char) = {
+    Vector(c1, c2, c3)
+  }
+
+  def explicit(c1: Char, c2: Char, c3: Char): Vector[Char] = {
+    Vector(c1, c2, c3)
+  }
+
+  def explicitDouble(c1: Double, c2: Double, c3: Double): Vector[Double] = {
+    Vector(c1, c2, c3)
+  }
+
+  def explicitList(c: Vector[Double]): List[Double] = {
+    var list: List[Double] = Nil
+    for(s <- c)
+      list = s :: list
+    list
+  }
+
+  def explicitSet(c: Vector[Double]): Set[Double] = {
+    var set: Set[Double] = Set.empty
+    for(s <- c)
+      set += s
+    set
+  }
+
+  def forecast(n: Int): String = {
+    n match {
+      case 100 => "Sunny"
+      case 80 => "Mostly Sunny"
+      case 50 => "Partly Sunny"
+      case 20 => "Mostly Cloudy"
+      case 0 => "Cloudy"
+      case _ => "Unknown"
+    }
+  }
+
+  def historicalData(data: Vector[Int]): String = {
+    var sunny = 0
+    var mostlySunny = 0
+    var partlySunny = 0
+    var mostlyCloudy = 0
+    var cloudy = 0
+
+    for(s <- data){
+      s match {
+        case 100 => sunny += 1
+        case 80 => mostlySunny += 1
+        case 50 => partlySunny += 1
+        case 20 => mostlyCloudy += 1
+        case 0 => cloudy += 1
+      }
+    }
+    s"Sunny=$sunny, Mostly Sunny=$mostlySunny, Partly Sunny=$partlySunny, Mostly Cloudy=$mostlyCloudy, Cloudy=$cloudy"
+  }
+
+
+  inferred('a', 'b', 'c') is "Vector(a, b, c)"
+  explicit('a', 'b', 'c') is "Vector(a, b, c)"
+
+
 }
