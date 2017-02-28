e7359371993c1d6b5be01d50d9b15f79776f69ed
johannventer2@gmail.com <rand0m>
2017-02-28 16:23:14 +0200

    AtomicScala Exercises - Added Example code. Exercises to follow

--- AtomicScala3FunctionsAsObjects.scala
+++ AtomicScala3FunctionsAsObjects.scala
@@ -6,8 +6,32 @@ import com.atomicscala.AtomicTest._
 
 object AtomicScala3FunctionsAsObjects {
   def main(args: Array[String]): Unit = {
+    val v = Vector(1, 2, 3, 4)
+    v.foreach(show)
+    v.foreach((n: Int) => println("> " + n))
+
+    //n: Int => println("> " + n)
+
+    val duck = "Duck".toVector
+    duck.foreach((n: Char) => println("> " + n))
+
+    var s = ""
+    val duck2 = "Duck".toVector
+    duck2.foreach((n: Char) => s = s + n + ":")
+    s is "D:u:c:k:"
+
+    val v2 = Vector(19, 1, 7, 3, 2, 14)
+    v2.sorted is Vector(1, 2, 3, 7, 14, 19)
+    v2.sortWith((i, j) => j < i) is Vector(19, 14, 7, 3, 2, 1)
+
+
 
   }
+
+  def show(n: Int): Unit = {
+    println("> " + n)
+  }
+
 
 
 }
