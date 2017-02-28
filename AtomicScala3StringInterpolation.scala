6cfee81da3d54bd3001e4a8fd6f56a48cf8584db
johannventer2@gmail.com <rand0m>
2017-02-28 14:58:16 +0200

    AtomicScala Exercises - Added StringInterpolation file. Added default main function. Example code to follow. Exercise code to follow.

---
+++ AtomicScala3StringInterpolation.scala
@@ -0,0 +1,11 @@
+/**
+  * Created by johann on 2017/02/28.
+  */
+
+import com.atomicscala.AtomicTest._
+
+object AtomicScala3StringInterpolation {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
