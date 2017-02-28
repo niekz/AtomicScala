6556a8dd1a8b0dd676a8b76a226f8c48640546ef
johannventer2@gmail.com <rand0m>
2017-02-28 16:44:53 +0200

    AtomicScala Exercises -Added exercise code

--- AtomicScala3FunctionsAsObjects.scala
+++ AtomicScala3FunctionsAsObjects.scala
@@ -24,6 +24,39 @@ object AtomicScala3FunctionsAsObjects {
     v2.sorted is Vector(1, 2, 3, 7, 14, 19)
     v2.sortWith((i, j) => j < i) is Vector(19, 14, 7, 3, 2, 1)
 
+    var s2 = ""
+    val str = "1234".toVector
+    str.foreach((n: Char) => s2 += n + ",")
+
+    s2 is "1,2,3,4,"
+
+    val dogYears = (n: Int) => n * 7
+    dogYears(10) is 70
+
+    var dogString = ""
+    val dogVector = Vector(1, 5, 7, 8)
+    dogVector.foreach((n: Int) => dogString += n * 7 + " ")
+
+    dogString is "7 35 49 56 "
+
+    def temp = (temp: Int, low: Int, high: Int) => if(temp > low && temp < high) true else false
+    temp(70, 80, 90) is false
+    temp(70, 60, 90) is true
+
+    var numberString = ""
+    val numbers = Vector(1, 2, 5, 3, 7)
+    numbers.foreach((n: Int) => numberString += n*n + " ")
+    numberString is "1 4 25 9 49 "
+
+    def pluralize = (word: String) => word + "s"
+    pluralize("cat") is "cats"
+    pluralize("dog") is "dogs"
+    pluralize("silly") is "sillys"
+
+    var wordsString = ""
+    val words = Vector("word", "cat", "animal")
+    words.foreach((word: String) => wordsString += pluralize(word) + " ")
+    wordsString is "words cats animals "
 
 
   }
