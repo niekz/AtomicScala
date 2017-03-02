9c42db942fb8f377813bf06fd53eab6de12a3f84
johannventer2@gmail.com <rand0m>
2017-03-02 12:18:21 +0200

    AtomicScala Exercises - Added example code. Added exercise code.

--- AtomicScala4CompanionObjects.scala
+++ AtomicScala4CompanionObjects.scala
@@ -15,6 +15,19 @@ object AtomicScala4CompanionObjects {
     X.n is 2
     X.f is 20
     X.g is 40
+
+    WalkActivity.start("TestName")
+
+    val sally = new WalkActivity
+    sally.calories(150, 30) is 82
+
+    val suzie = new WalkActivity
+    val john = new WalkActivity
+
+    suzie.calories2(150, 30) is 117
+    john.calories2(150, 30, 1.5) is 82
+
+
   }
 
   class X(val n: Int) {
@@ -25,5 +38,36 @@ object AtomicScala4CompanionObjects {
     val n = 2
     def f = n * 10
     def g = this.n * 20
+  }
+
+  class WalkActivity{
+    var MET = 2.3
+
+    def calories(lbs: Int, mins: Int, mph: Double = 3): Long = math.round(
+      (MET * 3.5 * lbs * 0.45)/200.0 * mins
+    )
+
+    def calories2(lbs: Int, mins: Int, mph: Double = 3): Long = math.round(
+      (MET(mph) * 3.5 * lbs * 0.45)/200.0 * mins
+    )
+    def MET(mph: Double) =
+      mph match {
+        case x: Double if(x < 1.7) => 2.3
+        case x: Double if(x < 2.5) => 2.9
+        case x: Double if(x < 3) => 3.3
+        case x: Double if(x >= 3) => 3.3
+        case _ => 2.3
+      }
+  }
+
+  object WalkActivity{
+    var log = ""
+
+    def start(name: String): Unit = {
+      log += s"[$name] Activity started."
+    }
+    def stop(name: String): Unit = {
+      log += s"[$name] Activity stopped."
+    }
   }
 }
