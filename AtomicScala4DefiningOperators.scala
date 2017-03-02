83f8753597b4f6138004134c85ce938d6a7b0137
johannventer2@gmail.com <rand0m>
2017-03-02 10:05:48 +0200

    AtomicScala Exercises -Added Defining Operators file. Exercise code to follow. Example code to be included.

---
+++ AtomicScala4DefiningOperators.scala
@@ -0,0 +1,13 @@
+/**
+  * Created by johann on 2017/03/02.
+  */
+
+import com.atomicscala.AtomicTest._
+
+object AtomicScala4DefiningOperators {
+  def main(args: Array[String]): Unit = {
+
+  }
+
+  
+}
