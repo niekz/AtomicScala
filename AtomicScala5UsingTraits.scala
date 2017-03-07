3f135344920fe4ed82a95596f2521f0aaeb133d6
johannventer2@gmail.com <rand0m>
2017-03-07 10:02:22 +0200

    AtomicScala Exercises - Added example code. Exercise code to follow.

--- AtomicScala5UsingTraits.scala
+++ AtomicScala5UsingTraits.scala
@@ -6,8 +6,33 @@ import com.atomicscala.AtomicTest._
 
 object AtomicScala5UsingTraits {
   def main(args: Array[String]): Unit = {
-
+    val bob = new Exerciser(44)
+    bob.isAerobic(180) is true
+    bob.isAerobic(80) is false
+    bob.minAerobic is 88.0
   }
+
+  trait Aerobic {
+    val age: Int
+    def minAerobic =   .5 * (220 - age)
+    def isAerobic(heartRate: Int) =
+      heartRate >= minAerobic
+  }
+
+  trait Activity {
+    val action: String
+    def go: String
+  }
+
+  class Person(val age: Int)
+
+  class Exerciser(age: Int,
+                  val action: String = "Running",
+                  val go: String = "Run!") extends Person(age)
+                                              with Activity
+                                              with Aerobic
+
+
 
 
 }
