baa0d41d245d827e09aafd856fcb1fd2f59cbaf3
johannventer2@gmail.com <rand0m>
2017-03-13 12:26:27 +0200

    AtomicScala Exercises - Added Design by Contract file. Example code to follow. Exercise code to follow.

---
+++ AtomicScala6DesignByContract.scala
@@ -0,0 +1,12 @@
+/**
+  * Created by johann on 2017/03/13.
+  */
+
+import com.atomicscala.AtomicTest._
+
+object AtomicScala6DesignByContract {
+
+  def main(args: Array[String]): Unit = {
+
+  }
+}
